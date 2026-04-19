411768551ce1e218aabc6ecc3eb2ef8b0a822b65
NedimHodzic04 <nedim.hodzic@stu.ibu.edu.ba>
2026-04-19 09:59:11 +0200

    Dependency fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,10 @@ import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
-import axios from "axios";
 import { Routes, Route, Link } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get("http://localhost:5000");
-      setMessage(response.data.message);
-    };
-    fetchData();
-  }, []);
-
   return (
     <>
       <h1>{message || null}</h1>
